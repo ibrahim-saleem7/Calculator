76b6bbd4ef083c3bc677630b52d66481a0bc3658
Ibrahim Saleem <ebrahim.salem@atos.net>
2023-06-02 23:44:30 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  display : string = '';
+  result : any ;
+
+  click(event :any){
+    this.display += event
+  }
+
+  equal(){
+    this.result =  eval(this.display)
+    if(Number.isInteger(this.result)){
+      this.display = this.result
+    }else {
+      this.display = this.result.toFixed(2)
+    }
+  }
+
+  clear(){
+    this.display = ''
+  }
+
+  erase(){
+    this.display = this.display.replace(this.display[this.display.length-1]  , "")
+  }
+
+
+
   title = 'Calculator';
 }
+
